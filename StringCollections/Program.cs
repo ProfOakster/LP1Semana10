76a305b0bd08cf7791ab82786ef5b46e169a2a2f
TomasCarvalho <2606tom@gmail.com>
2023-05-19 11:50:25 +0100

    Create project StringCollections

---
+++ StringCollections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StringCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> list = new List<string>() {
+                "Quagsire", "Lando-T","T-Tar", "Lucario", "Quagsire" };
+            Stack<String> stack = new Stack<string>() { 
+                "Quagsire", "Lando-T","T-Tar", "Lucario", "Quagsire"};
+            Queue<String> queue = new Queue<string>() {
+                "Quagsire", "Lando-T","T-Tar", "Lucario", "Quagsire" };
+            HashSet<String> hashset = new HashSet<string>() {
+                "Quagsire", "Lando-T","T-Tar", "Lucario", "Quagsire" };
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
